bb12cf97cf9a8afbda28e053d03fa7ec85e3397e
Ihor Horchynskyi <i.horchynskyi@gmail.com>
2020-02-24 23:50:54 +0200

    It works

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
+        System.out.println("INIT");
         List<String> firstMtdList = Arrays.asList(
                 "xml/ReportPartition.TransactionsFees_Part1.md-meta.xml",
                 "xml/ReportPartition.TransactionsFees_Part2.md-meta.xml",
@@ -19,10 +19,14 @@ public class Main {
             ReportPartitionMetadata report = XmlParser.deserialize(filePath);
             metadataObjectsList.add(report);
         }
-
         for (int i = 1; i < metadataObjectsList.size(); i++) {
             for (String fieldName : metadataObjectsList.get(0).getMtdMap().keySet()) {
-                metadataObjectsList.get(i).getMtdMap().remove(fieldName);
+                System.out.print("Report " + metadataObjectsList.get(i).getLabel() + ": " + "Field \"" + fieldName + "\"");
+                if( metadataObjectsList.get(i).getMtdMap().containsKey(fieldName)) {
+                    System.out.print(" removed!");
+                    metadataObjectsList.get(i).getMtdMap().remove(fieldName);
+                }
+                System.out.println(" ");
             }
         }
 
@@ -35,8 +39,16 @@ public class Main {
         for (int i = 0; i < metadataObjectsList.size(); i++) {
             ReportPartitionMetadata report = metadataObjectsList.get(i);
             for (FieldValuePair pair : report.getValues()) {
-                if(pair.field == "ColumnNames__c") pair.value = String.join(",", report.getMtdMap().keySet());
-                if(pair.field == "ReportFields__c") pair.value = String.join(",", report.getMtdMap().values());
+                if(pair.field.equals("ColumnNames__c")){
+                    List<String> uniqueNames = new ArrayList<>();
+                    for (String name : report.getMtdMap().keySet()){
+                        if( report.getMtdMap().get(name) != null) uniqueNames.add(name);
+                    }
+                    System.out.println("uniqueNames " + uniqueNames.size());
+                    pair.value = String.join(",", uniqueNames);
+                }
+
+                if(pair.field.equals("ReportFields__c")) pair.value = String.join(",", report.getMtdMap().values());
             }
 
             XmlParser.serialize(metadataObjectsList.get(i), exportMtdList.get(i));
--- src/main/java/ReportPartitionMetadata.java
+++ src/main/java/ReportPartitionMetadata.java
@@ -33,20 +33,24 @@ public class ReportPartitionMetadata {
 
     public Map<String, String> getMtdMap() {
         if (mtdMap == null) {
-            System.out.println("This is " + this);
             mtdMap = new HashMap<>();
 
             List<String> fieldNameList = new ArrayList<>();
             List<String> fieldValueList = new ArrayList<>();
             for (FieldValuePair pair : values) {
-                if(pair.field == "ColumnNames__c") fieldNameList = Arrays.asList(pair.value.split(","));
-                if(pair.field == "ReportFields__c") fieldValueList = Arrays.asList(pair.value.split(","));
+                if(pair.field.equals("ColumnNames__c")) {
+                    fieldNameList = Arrays.asList(pair.value.split(","));
+                }
+                if(pair.field.equals("ReportFields__c")) {
+                    fieldValueList = Arrays.asList(pair.value.split(","));
+                }
             }
 
             for(int i = 0; i<fieldNameList.size(); i++){
                 mtdMap.put(fieldNameList.get(i), fieldValueList.get(i));
+                //System.out.println(fieldNameList.get(i) + " = " + fieldValueList.get(i));
             }
-
+            System.out.println("Map size = " + mtdMap.size());
         }
         return mtdMap;
     }
